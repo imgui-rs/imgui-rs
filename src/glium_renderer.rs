550b8d1c1c408d026517db5fb64af0f3ae71d433
Joonas Javanainen <joonas.javanainen@gmail.com>
2016-03-26 18:42:39 +0200

    Fix uniform texture naming
    The shaders refer to the texture with name "tex", and the name must
    match on the Rust code side.
    
    Fixes #17

--- src/glium_renderer.rs
+++ src/glium_renderer.rs
@@ -99,7 +99,7 @@ impl Renderer {
             ];
             let uniforms = uniform! {
                 matrix: matrix,
-                texture: self.device_objects.texture.sampled()
+                tex: self.device_objects.texture.sampled()
             };
             let draw_params = DrawParameters {
                 blend: Blend::alpha_blending(),
