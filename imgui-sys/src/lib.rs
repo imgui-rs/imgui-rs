286c54a0df178511fc77c16d7cd7c44a3776f6f0
Malik Olivier Boussejra <malik@boussejra.com>
2018-04-30 12:30:42 +0900

    [cimgui 1.53.1] ImGuiIO: Add opt_cursor_blink

--- imgui-sys/src/lib.rs
+++ imgui-sys/src/lib.rs
@@ -561,6 +561,7 @@ pub struct ImGuiIO {
     pub display_visible_max: ImVec2,
 
     pub osx_behaviors: bool,
+    pub opt_cursor_blink: bool,
 
     pub render_draw_lists_fn: Option<extern "C" fn(data: *mut ImDrawData)>,
 
