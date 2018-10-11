b1c7a034156a307157a5e70be06c647c2e792e9b
Joonas Javanainen <joonas.javanainen@gmail.com>
2018-10-11 23:10:54 +0300

    Use cimgui 1.65.2

--- imgui-sys/build.rs
+++ imgui-sys/build.rs
@@ -3,12 +3,10 @@ extern crate cc;
 fn main() {
     cc::Build::new()
         .cpp(true)
-        .file("third-party/cimgui/cimgui/cimgui.cpp")
-        .file("third-party/cimgui/cimgui/fontAtlas.cpp")
-        .file("third-party/cimgui/cimgui/drawList.cpp")
-        .file("third-party/cimgui/cimgui/listClipper.cpp")
+        .file("third-party/cimgui/cimgui.cpp")
         .file("third-party/cimgui/imgui/imgui.cpp")
         .file("third-party/cimgui/imgui/imgui_demo.cpp")
         .file("third-party/cimgui/imgui/imgui_draw.cpp")
+        .file("third-party/cimgui/imgui/imgui_widgets.cpp")
         .compile("libcimgui.a");
 }
