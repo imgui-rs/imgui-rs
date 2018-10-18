6e4d2b2bbfdfacf924b7e49cabf73de247e87819
Joonas Javanainen <joonas.javanainen@gmail.com>
2018-10-18 22:29:31 +0300

    Version function

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,22 @@ mod style;
 #[cfg(test)]
 mod test;
 
+use std::ffi::CStr;
+use std::str;
+
 pub use context::*;
 pub use string::*;
 pub use style::*;
+
+/// Returns the underlying Dear ImGui library version
+pub fn get_dear_imgui_version() -> &'static str {
+    unsafe {
+        let bytes = CStr::from_ptr(sys::igGetVersion()).to_bytes();
+        str::from_utf8_unchecked(bytes)
+    }
+}
+
+#[test]
+fn test_get_version() {
+    assert_eq!(get_dear_imgui_version(), "1.67");
+}
