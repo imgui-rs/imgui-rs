3aebc164602ba024262ba585aa57b548a98149cd
Chris Frantz <frantzcj@gmail.com>
2021-04-12 22:13:00 -0700

    Update built cimgui files
    cimgui 6a98f4bbedec967d02f3395f319feceec311ecb2
    imgui e87dd0e65dd2786c50a7cfa411d0448140ffc8f1

---
+++ imgui-sys/third-party/cimgui_impl.h
@@ -0,0 +1,38 @@
+typedef struct SDL_Window SDL_Window;
+typedef struct GLFWwindow GLFWwindow;
+
+struct GLFWwindow;
+
+struct SDL_Window;
+typedef union SDL_Event SDL_Event;CIMGUI_API bool ImGui_ImplGlfw_InitForOpenGL(GLFWwindow* window,bool install_callbacks);
+CIMGUI_API bool ImGui_ImplGlfw_InitForVulkan(GLFWwindow* window,bool install_callbacks);
+CIMGUI_API bool ImGui_ImplGlfw_InitForOther(GLFWwindow* window,bool install_callbacks);
+CIMGUI_API void ImGui_ImplGlfw_Shutdown();
+CIMGUI_API void ImGui_ImplGlfw_NewFrame();
+CIMGUI_API void ImGui_ImplGlfw_MouseButtonCallback(GLFWwindow* window,int button,int action,int mods);
+CIMGUI_API void ImGui_ImplGlfw_ScrollCallback(GLFWwindow* window,double xoffset,double yoffset);
+CIMGUI_API void ImGui_ImplGlfw_KeyCallback(GLFWwindow* window,int key,int scancode,int action,int mods);
+CIMGUI_API void ImGui_ImplGlfw_CharCallback(GLFWwindow* window,unsigned int c);
+CIMGUI_API bool ImGui_ImplOpenGL3_Init(const char* glsl_version);
+CIMGUI_API void ImGui_ImplOpenGL3_Shutdown();
+CIMGUI_API void ImGui_ImplOpenGL3_NewFrame();
+CIMGUI_API void ImGui_ImplOpenGL3_RenderDrawData(ImDrawData* draw_data);
+CIMGUI_API bool ImGui_ImplOpenGL3_CreateFontsTexture();
+CIMGUI_API void ImGui_ImplOpenGL3_DestroyFontsTexture();
+CIMGUI_API bool ImGui_ImplOpenGL3_CreateDeviceObjects();
+CIMGUI_API void ImGui_ImplOpenGL3_DestroyDeviceObjects();
+CIMGUI_API bool ImGui_ImplOpenGL2_Init();
+CIMGUI_API void ImGui_ImplOpenGL2_Shutdown();
+CIMGUI_API void ImGui_ImplOpenGL2_NewFrame();
+CIMGUI_API void ImGui_ImplOpenGL2_RenderDrawData(ImDrawData* draw_data);
+CIMGUI_API bool ImGui_ImplOpenGL2_CreateFontsTexture();
+CIMGUI_API void ImGui_ImplOpenGL2_DestroyFontsTexture();
+CIMGUI_API bool ImGui_ImplOpenGL2_CreateDeviceObjects();
+CIMGUI_API void ImGui_ImplOpenGL2_DestroyDeviceObjects();
+CIMGUI_API bool ImGui_ImplSDL2_InitForOpenGL(SDL_Window* window,void* sdl_gl_context);
+CIMGUI_API bool ImGui_ImplSDL2_InitForVulkan(SDL_Window* window);
+CIMGUI_API bool ImGui_ImplSDL2_InitForD3D(SDL_Window* window);
+CIMGUI_API bool ImGui_ImplSDL2_InitForMetal(SDL_Window* window);
+CIMGUI_API void ImGui_ImplSDL2_Shutdown();
+CIMGUI_API void ImGui_ImplSDL2_NewFrame(SDL_Window* window);
+CIMGUI_API bool ImGui_ImplSDL2_ProcessEvent(const SDL_Event* event);
--- imgui-sys/third-party/update-cimgui-output.sh
+++ imgui-sys/third-party/update-cimgui-output.sh
@@ -7,7 +7,8 @@ CIMGUI_DIR=${1:?}
 echo "${SCRIPT_DIR}"
 
 pushd "${CIMGUI_DIR}"/generator > /dev/null
-luajit generator.lua gcc false -DIMGUI_USE_WCHAR32
+## luajit generator.lua gcc false -DIMGUI_USE_WCHAR32
+luajit generator.lua gcc internal -DIMGUI_USE_WCHAR32
 popd > /dev/null
 
 cp "${CIMGUI_DIR}"/cimgui.{h,cpp} "${SCRIPT_DIR}"/
