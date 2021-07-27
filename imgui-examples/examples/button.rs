b8ba74117808d75461dcefabee1499335fdb47e7
Scott Lee <scott.lee@engie.com>
2021-07-26 21:28:13 -0500

    Updated example with variables.

--- imgui-examples/examples/button.rs
+++ imgui-examples/examples/button.rs
@@ -10,14 +10,18 @@ fn swap_button_text(show_button_text: &bool) -> bool
 }
 
 fn main() {
+
+    let default_width: &f32 = &50.0;
+    let default_height: &f32 = &32.0;
+
     let mut show_button_text = false;
     let system = support::init(file!());
     system.main_loop(move |_, ui| {
         Window::new(im_str!("Button Example"))
             .size([300.0, 110.0], Condition::FirstUseEver)
             .build(ui, || {
-                let button_clicked = imgui::Button::new("Test", &50.0f32, &32.0f32).build();
-                if button_clicked { show_button_text = swap_button_text(&show_button_text); }
+                let basic_button_clicked = imgui::Button::new("Test", default_width, default_height).build();
+                if basic_button_clicked { show_button_text = swap_button_text(&show_button_text); }
                 if show_button_text { ui.text(im_str!("You clicked a button!")); }
             });
     });
