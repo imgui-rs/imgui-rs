777cefde03c4140340ba310ef77b181f80549eb8
Jack Mac <jjspira@gmail.com>
2021-10-19 20:21:12 -0400

    imgui text

--- imgui-examples/examples/text_input.rs
+++ imgui-examples/examples/text_input.rs
@@ -5,6 +5,7 @@ mod support;
 fn main() {
     let system = support::init(file!());
     let mut stable_str = String::new();
+    let mut callback_str = String::new();
 
     system.main_loop(move |_, ui| {
         if let Some(_window) = ui
@@ -21,6 +22,25 @@ fn main() {
 
             if ui.is_item_deactivated_after_edit() {
                 dbg!(&per_frame_buf);
+            }
+
+            struct CB;
+            impl imgui::InputTextCallbackHandler for CB {
+                fn on_history(
+                    &mut self,
+                    _dir: imgui::HistoryDirection,
+                    _data: imgui::TextCallbackData,
+                ) {
+                }
+            }
+            let changed = ui
+                .input_text("input callback", &mut callback_str)
+                .callback(InputTextCallback::HISTORY, CB)
+                .enter_returns_true(true)
+                .build();
+
+            if changed {
+                println!("{:?}", callback_str);
             }
         }
     });
