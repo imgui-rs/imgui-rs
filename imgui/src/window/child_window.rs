e522bcf123592272a73c01f3a49492ff249e9ba4
dbr <dbr.onix@gmail.com>
2021-11-16 19:41:45 +1100

    fmt

--- imgui/src/window/child_window.rs
+++ imgui/src/window/child_window.rs
@@ -21,10 +21,7 @@ pub struct ChildWindow<'ui> {
 
 impl<'ui> ChildWindow<'ui> {
     /// Creates a new child window builder with the str.
-    #[deprecated(
-        since = "0.9.0",
-        note = "use ui.child_window(...) instead"
-    )]
+    #[deprecated(since = "0.9.0", note = "use ui.child_window(...) instead")]
     #[doc(alias = "BeginChildID")]
     pub fn new(ui: &'ui Ui, name: impl AsRef<str>) -> Self {
         let id = ui.new_id_str(name);
