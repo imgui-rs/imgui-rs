428918469b96f286ae2910229cd0cc1a8adb2b38
Jonathan Spira <jjspira@gmail.com>
2024-09-29 23:41:38 -0400

    getting new cpp files

--- imgui-sys/third-party/update-imgui.sh
+++ imgui-sys/third-party/update-imgui.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
+# this should be a path on your local, like `~/Documents/imgui` to the Dear ImGui repository.
+IMGUI_DIR=${1:?}
+
 SCRIPT_DIR=$(dirname ${0} | python3 -c 'import os, sys; print(os.path.abspath(sys.stdin.read().strip()))' )
 
 cd ${SCRIPT_DIR}
-./_update-imgui.sh ~/code/vendor/imgui v1.89.2 ./imgui-master/imgui
-./_update-imgui.sh ~/code/vendor/imgui d822c65317ba881798bed8fce9ffba267d27dada ./imgui-docking/imgui
+./_update-imgui.sh $IMGUI_DIR v1.91.2 ./imgui-master/imgui
+./_update-imgui.sh $IMGUI_DIR v1.91.2-docking ./imgui-docking/imgui
 
-./_update-imgui.sh ~/code/vendor/imgui v1.89.2 ./imgui-master-freetype/imgui
-./_update-imgui.sh ~/code/vendor/imgui d822c65317ba881798bed8fce9ffba267d27dada ./imgui-docking-freetype/imgui
+./_update-imgui.sh $IMGUI_DIR v1.91.2 ./imgui-master-freetype/imgui
+./_update-imgui.sh $IMGUI_DIR v1.91.2-docking ./imgui-docking-freetype/imgui
