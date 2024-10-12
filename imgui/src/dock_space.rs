91fdf45397ec3bd77423900659c33d816bf61f00
Roy Jacobs <roy.jacobs@gmail.com>
2024-10-12 17:07:39 +0200

    Return the dockspace ID from `dockspace_over_main_viewport`

--- imgui/src/dock_space.rs
+++ imgui/src/dock_space.rs
@@ -3,14 +3,14 @@ use std::ptr::null;
 use crate::Ui;
 
 impl Ui {
-    pub fn dockspace_over_main_viewport(&self) {
+    pub fn dockspace_over_main_viewport(&self) -> imgui_sys::ImGuiID {
         unsafe {
             sys::igDockSpaceOverViewport(
                 0,
                 sys::igGetMainViewport(),
                 sys::ImGuiDockNodeFlags_PassthruCentralNode as i32,
                 null(),
-            );
+            )
         }
     }
 }
