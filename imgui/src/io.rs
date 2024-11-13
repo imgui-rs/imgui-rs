36dd72e72dd022992353159127aee3df75b6bcf8
jestarray <34615798+jestarray@users.noreply.github.com>
2024-11-13 09:42:49 -0800

    document `add_focus_event`

--- imgui/src/io.rs
+++ imgui/src/io.rs
@@ -457,7 +457,9 @@ impl Io {
             sys::ImGuiIO_AddKeyEvent(self.raw_mut(), key as u32, down);
         }
     }
-
+    // Queue a gain/loss of focus for the application (generally based on OS/platform focus of your window)
+    // Note: [`io.config_debug_ignore_focus_loss`] will ignore this event from firing
+    #[doc(alias = "AddFocusEvent")
     pub fn add_focus_event(&mut self, focused: bool) {
         unsafe {
             sys::ImGuiIO_AddFocusEvent(self.raw_mut(), focused);
